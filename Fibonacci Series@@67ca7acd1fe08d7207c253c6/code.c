79c0bc5f2aa9ca15c94f150305a9dbd4ba921def
hardiksaluja1 <hardik1306.becse24@chitkara.edu.in>
2025-03-29 12:31:48 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,39 +1,14 @@
-#include <stdio.h>
-
-void fibonacci(int n) {
-    int first = 0, second = 1, next;
-    int i = 1; // Start with the first term
-
-    // Print the first Fibonacci number if n >= 1
-    while (i <= n) {
-        if (i == 1) {
-            printf("%d ", first); // Print the first term
-        } else if (i == 2) {
-            printf("%d ", second); // Print the second term
-        } else {
-            next = first + second;
-            printf("%d ", next); // Print the next Fibonacci number
-
-            // Update first and second for the next iteration
-            first = second;
-            second = next;
-        }
-
-        i++; // Increment the counter
-    }
-}
-
-int main() {
+#include<stdio.h>
+int main(){
     int n;
-    scanf("%d", &n);
-
-    // Calling the Fibonacci function to print the sequence
-    printf("%d ", n);
-    fibonacci(n);
-
+    scanf("%d",&n);
+    if(n<=0){
+        printf("Invalid input\n");
+        return 0;
+    }
+    fibonacciseries(n);
     return 0;
 }
-
 
 
 
